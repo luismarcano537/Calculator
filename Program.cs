a858c5653a0dd5770c6b058fc7bb0791931d7d46
Luis Silva <luissilvam537@gmail.com>
2026-02-09 11:45:20 -0300

    Initial project structure

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float firstValue = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float secondValue = float.Parse(Console.ReadLine());
+            
+            float result = firstValue + secondValue;
+
+            Console.WriteLine($"A soma dos valores informados é: {result}");
+        }
+    }
+}
