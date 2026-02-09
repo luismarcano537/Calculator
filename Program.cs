3b57c43217936395f7ec54c72925a3e8c5e1a42f
Luis Silva <luissilvam537@gmail.com>
2026-02-09 13:59:28 -0300

    Creating the main functions of the program

--- Program.cs
+++ Program.cs
@@ -5,16 +5,74 @@
         static void Main(string[] args)
         {
             Console.Clear();
+            Sum();
+            Subtract();
+            Division();
+            Multiplication();
+        }
 
-            Console.WriteLine("Primeiro valor: ");
+        static void Sum()
+        {
+            Console.Clear();
+            Console.WriteLine("Soma: ");
+            Console.Write("Primeiro valor: ");
             float firstValue = float.Parse(Console.ReadLine());
 
-            Console.WriteLine("Segundo valor: ");
+            Console.Write("Segundo valor: ");
             float secondValue = float.Parse(Console.ReadLine());
             
             float result = firstValue + secondValue;
 
             Console.WriteLine($"A soma dos valores informados é: {result}");
+            Console.ReadKey();
+        }
+
+        static void Subtract()
+        {
+            Console.Clear();
+            Console.WriteLine("Subtração: ");
+            Console.Write("Primeiro valor: ");
+            float firstValue = float.Parse(Console.ReadLine());
+
+            Console.Write("Segundo valor: ");
+            float secondValue = float.Parse(Console.ReadLine());
+            
+            float result = firstValue - secondValue;
+
+            Console.WriteLine($"A subtração dos valores informados é: {result}");
+            Console.ReadKey();
+        }
+
+        static void Division()
+        {
+            Console.Clear();
+            Console.WriteLine("Divisão: ");
+            Console.Write("Primeiro valor: ");
+            float firstValue = float.Parse(Console.ReadLine());
+
+            Console.Write("Segundo valor: ");
+            float secondValue = float.Parse(Console.ReadLine());
+            
+            float result = firstValue / secondValue;
+
+            Console.WriteLine($"A divisão dos valores informados é: {result}");
+            Console.ReadKey();
+        }
+
+        static void Multiplication()
+        {
+            Console.Clear();
+            Console.WriteLine("Multiplicacao: ");
+            Console.Write("Primeiro valor: ");
+            float firstValue = float.Parse(Console.ReadLine());
+
+            Console.Write("Segundo valor: ");
+            float secondValue = float.Parse(Console.ReadLine());
+            
+            float result = firstValue * secondValue;
+
+            Console.WriteLine($"A multiplicação dos valores informados é: {result}");
+            Console.ReadKey();
         }
     }
 }
